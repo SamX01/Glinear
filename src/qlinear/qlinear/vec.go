7cb5b1149ea1ec812c30053d2b2f9f44dc2e42da
XP <595622836@qq.com>
2020-07-18 01:23:07 +0800

    add a simple test

--- src/qlinear/qlinear/vec.go
+++ src/qlinear/qlinear/vec.go
@@ -7,21 +7,25 @@ type Vec3f struct {
 	F3 float32
 }
 
-type Vec2f struct {
-	F1 float32
-	F2 float32
-}
-
 type Vec3i struct {
 	I1 int8
 	I2 int8
 	I3 int8
 }
 
+/*二维Vec*/
+
+type Vec2f struct {
+	F1 float32
+	F2 float32
+}
+
 type Vec2i struct {
 	I1 int8
 	I2 int8
 }
+
+/*多维Vec*/
 
 type Veci struct {
 	I []int8
--- src/qlinear/qlinear/vecMethod.go
+++ src/qlinear/qlinear/vecMethod.go
@@ -2,21 +2,13 @@ package qlinear
 
 import "math"
 
-func (u Vec3f) crossProduct(v Vec3f) Vec3f {
-	return Vec3f{F1: u.F2*v.F3 - u.F3*v.F2,
-		F2: u.F3*v.F1 - u.F1*v.F3,
-		F3: u.F1*v.F2 - u.F2*v.F1,
-	}
-}
 
-func (u Vec3f) innerProduct(v Vec3f) float32 {
-	return u.F1*v.F1 + u.F2*v.F2 + u.F3*v.F3
-}
 
 func (x Vec3f) norm() float32 {
 	return sqrt(x.F1*x.F1 + x.F2*x.F2 + x.F3*x.F3)
 }
 
+//float32的开方
 func sqrt(x float32) float32 {
 	var xhalf float32 = 0.5 * x // get bits for floating VALUE
 	i := math.Float32bits(x)    // gives initial guess y0
